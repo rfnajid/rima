0d65d718519085b0a5193291a13a34f27c4355cb
Ridhazis Faranto Najid <ridhaaaaazis@gmail.com>
2020-08-22 04:47:57 +0700

    fix %20 in search

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,7 +1,7 @@
 <a href="/"><h1 class="title">rima</h1></a>
 <form action="/" method="post">
     <div class="form-group search">
-        <input required type="text" name="search" class="form-control form-control-lg" placeholder="Cari rimanya disini..." value="{!! $query!!}">
+        <input required type="text" name="search" class="form-control form-control-lg" placeholder="Cari rimanya disini..." value="{!!urldecode($query)!!}">
         <button class="btn fa fa-search"></button>
     </div>
 </form>
