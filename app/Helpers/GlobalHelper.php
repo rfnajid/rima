42ab29a055a467ec50d76f28126b768e902a0183
Ridhazis Faranto Najid <ridhaaaaazis@gmail.com>
2020-08-29 21:43:57 +0700

    add public_path helper

--- app/Helpers/GlobalHelper.php
+++ app/Helpers/GlobalHelper.php
@@ -4,13 +4,6 @@ namespace App\Helpers;
 
 
 class GlobalHelper {
-    public static function getColumn(){
-        return ['id','kata','arti'];
-    }
-
-    public static function getPagination(){
-        return 17;
-    }
 
     public static function log($str){
         error_log(print_r($str, TRUE));
