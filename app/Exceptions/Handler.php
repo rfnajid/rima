996b0ab38a62ba69d503360c54104eb8f6209c1b
Ridhazis Faranto Najid <ridhaaaaazis@gmail.com>
2020-08-29 07:35:25 +0700

    add page 500

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -10,6 +10,7 @@ use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+
 class Handler extends ExceptionHandler
 {
     /**
@@ -48,6 +49,8 @@ class Handler extends ExceptionHandler
     {
         if($exception instanceof NotFoundHttpException){
             return response(view('errors.404'),404);
+        }else if(env('APP_DEBUG') === false){
+            return response(view('errors.500'),500);
         }
 
         return parent::render($request, $exception);
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,8 +3,8 @@
     <body>
         <main>
             <i class="far fa-frown fa-7x"></i>
-            <h1>404</h1>
-            <h2>Halaman Tidak Ditemukan</h2>
+            <h1>500</h1>
+            <h2>ooops... error...</h2>
             <a class="btn" href="{{url('')}}"><i class="fa fa-home fa-2x"></i></a>
         </main>
     </body>
